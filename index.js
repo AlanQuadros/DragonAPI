bbf88fc11e4850e2ff4c681f2e28a306b236016a
Alan Quadros <alan@stargrid.pro>
2021-06-17 17:54:31 -0300

    Adjusts

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get("/", (req, res) =>{
 
 require("./app/routes/dragon.routes.js")(app);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server running in port 3000")
 })
